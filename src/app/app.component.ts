ac277409609cf8363a4544185688a4afeab03420
Nikhil Sachan <nikhilsachan159@gmail.com>
2025-03-09 19:55:36 +0530

    Setup Auto reloding for development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Eureka-Enterprises-Electrical-Frontend';
+  angularVersion = VERSION.full; // Fetches Angular version
 }
