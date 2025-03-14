30ed348d9b8760279a2b7bcea013843db47bff7b
Nikhil Sachan <nikhilsachan159@gmail.com>
2025-03-14 20:56:20 +0530

    Setuped Tailwind + NgPrime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, VERSION } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HasPermissionDirective } from './shared/directives/has-permission.directive';
 import { PERMISSIONS } from './core/constants/permission.constants';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [HasPermissionDirective],
+    imports: [RouterOutlet, HasPermissionDirective, ButtonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,30 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false || 'none',
+          cssLayer: {
+            name: 'primeng',
+            order: 'tailwind, primeng',
+          },
+        }
+      }
+    }),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(
       withInterceptors([AuthInterceptor, ErrorInterceptor, LoggingInterceptor]),
     ),
