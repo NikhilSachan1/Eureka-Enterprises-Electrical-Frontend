2f0eed9d95820396a80f84926c4cd89a69c595f5
Nikhil Sachan <nikhilsachan159@gmail.com>
2025-05-18 08:12:41 +0530

    Remove unused things

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,51 +8,6 @@ export const routes: Routes = [
       .then(m => m.EMPLOYEE_MANAGEMENT_ROUTES)
   },
   {
-    path: ROUTE_BASE_PATHS.ATTENDANCE,
-    loadChildren: () => import('./features/attendance-management/attendance-management.routes')
-      .then(m => m.ATTENDANCE_MANAGEMENT_ROUTES)
-  },
-  {
-    path: ROUTE_BASE_PATHS.CARD,
-    loadChildren: () => import('./features/card-management/card-management.routes')
-      .then(m => m.CARD_MANAGEMENT_ROUTES)
-  },
-  {
-    path: ROUTE_BASE_PATHS.LEAVE,
-    loadChildren: () => import('./features/employee-leaves-management/employee-leaves-management.routes')
-      .then(m => m.EMPLOYEE_LEAVES_MANAGEMENT_ROUTES)
-  },
-  {
-    path: ROUTE_BASE_PATHS.EXPENSE,
-    loadChildren: () => import('./features/regular-expense-management/regular-expense-management.routes')
-      .then(m => m.REGULAR_EXPENSE_MANAGEMENT_ROUTES)
-  },
-  {
-    path: ROUTE_BASE_PATHS.FUEL,
-    loadChildren: () => import('./features/fuel-expense-management/fuel-expense-management.routes')
-      .then(m => m.FUEL_EXPENSE_MANAGEMENT_ROUTES)
-  },
-  {
-    path: ROUTE_BASE_PATHS.CALENDAR,
-    loadChildren: () => import('./features/holiday-calendar-management/holiday-calendar-management.routes')
-      .then(m => m.HOLIDAY_CALENDAR_MANAGEMENT_ROUTES)
-  },
-  {
-    path: ROUTE_BASE_PATHS.VEHICLE,
-    loadChildren: () => import('./features/vehicle-management/vehicle-management.routes')
-      .then(m => m.VEHICLE_MANAGEMENT_ROUTES)
-  },
-  {
-    path: ROUTE_BASE_PATHS.ASSET,
-    loadChildren: () => import('./features/asset-management/asset-management.routes')
-      .then(m => m.ASSET_MANAGEMENT_ROUTES)
-  },
-  {
-    path: ROUTE_BASE_PATHS.PAYROLL,
-    loadChildren: () => import('./features/payroll-management/payroll-management.routes')
-      .then(m => m.PAYROLL_MANAGEMENT_ROUTES)
-  },
-  {
     path: '**',
     redirectTo: ROUTE_BASE_PATHS.DASHBOARD
   }
