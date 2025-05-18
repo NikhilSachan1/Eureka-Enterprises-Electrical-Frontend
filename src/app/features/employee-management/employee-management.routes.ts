9fde199220504ba70228af2629ef08f6a0ee88cd
Nikhil Sachan <nikhilsachan159@gmail.com>
2025-05-18 08:14:08 +0530

    Remove unused things

--- src/app/features/employee-management/employee-management.routes.ts
+++ src/app/features/employee-management/employee-management.routes.ts
@@ -7,16 +7,4 @@ export const EMPLOYEE_MANAGEMENT_ROUTES: Routes = [
     loadComponent: () => import('./employee-list/employee-list.component')
       .then(m => m.EmployeeListComponent)
   },
-  {
-    path: ROUTES.EMPLOYEE.ADD,
-    loadComponent: () => import('./add-edit-employee/add-edit-employee.component')
-      .then(m => m.AddEditEmployeeComponent)
-  },
-  {
-    path: ROUTES.EMPLOYEE.EDIT,
-    loadComponent: () => import('./add-edit-employee/add-edit-employee.component')
-      .then(m => m.AddEditEmployeeComponent)
-  },
-
-
 ]; 
