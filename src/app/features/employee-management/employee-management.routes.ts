b3732ecf933a37651c6d69f0f1e4b8f5e81af790
Nikhil Sachan <nikhilsachan159@gmail.com>
2025-05-25 18:06:22 +0530

    Add employee setup done

--- src/app/features/employee-management/employee-management.routes.ts
+++ src/app/features/employee-management/employee-management.routes.ts
@@ -7,4 +7,9 @@ export const EMPLOYEE_MANAGEMENT_ROUTES: Routes = [
     loadComponent: () => import('./employee-list/employee-list.component')
       .then(m => m.EmployeeListComponent)
   },
+  {
+    path: ROUTES.EMPLOYEE.ADD,
+    loadComponent: () => import('./employee-add/employee-add.component')
+      .then(m => m.EmployeeAddComponent)
+  },
 ]; 
