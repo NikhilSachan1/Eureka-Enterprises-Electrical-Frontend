254453eb3bbd0d162325484e28db4041f1c00a7c
Nikhil Sachan <nikhilsachan159@gmail.com>
2025-06-07 23:29:52 +0530

    Manage Public & Private Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,42 @@
 import { Routes } from '@angular/router';
 import { ROUTE_BASE_PATHS } from './shared/constants/index';
+import { PublicLayoutComponent } from './shared/components/layouts/public-layout/public-layout.component';
+import { PrivateLayoutComponent } from './shared/components/layouts/private-layout/private-layout.component';
 
 export const routes: Routes = [
-  {
-    path: ROUTE_BASE_PATHS.AUTH,
-    loadChildren: () => import('./features/auth/auth.routes')
-      .then(m => m.AUTH_ROUTES)
-  },
-  {
-    path: ROUTE_BASE_PATHS.EMPLOYEE,
-    loadChildren: () => import('./features/employee-management/employee-management.routes')
-      .then(m => m.EMPLOYEE_MANAGEMENT_ROUTES)
-  },
+  // Public routes (no sidebar)
   {
     path: '',
-    redirectTo: ROUTE_BASE_PATHS.AUTH,
-    pathMatch: 'full'
+    component: PublicLayoutComponent,
+    children: [
+      {
+        path: ROUTE_BASE_PATHS.AUTH,
+        loadChildren: () => import('./features/auth/auth.routes')
+          .then(m => m.AUTH_ROUTES)
+      },
+      {
+        path: '',
+        redirectTo: ROUTE_BASE_PATHS.AUTH,
+        pathMatch: 'full'
+      }
+    ]
   },
+  
+  // Private routes (with sidebar)
+  {
+    path: '',
+    component: PrivateLayoutComponent,
+    children: [
+      {
+        path: ROUTE_BASE_PATHS.EMPLOYEE,
+        loadChildren: () => import('./features/employee-management/employee-management.routes')
+          .then(m => m.EMPLOYEE_MANAGEMENT_ROUTES)
+      },
+    ]
+  },
+  
   {
     path: '**',
-    redirectTo: ROUTE_BASE_PATHS.DASHBOARD
+    redirectTo: ROUTE_BASE_PATHS.AUTH
   }
 ];
---
+++ src/app/shared/components/layouts/private-layout/private-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-private-layout',
+  imports: [SidebarComponent, RouterOutlet],
+  templateUrl: './private-layout.component.html',
+  styleUrl: './private-layout.component.scss'
+})
+export class PrivateLayoutComponent {
+
+}
