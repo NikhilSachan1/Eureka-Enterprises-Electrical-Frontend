586d6733b3bff7e2ad0438f249680cd253c5d10c
Nikhil Sachan <nikhilsachan159@gmail.com>
2025-06-24 18:20:50 +0530

    Removed unimplemented compoenents/ module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { ROUTE_BASE_PATHS, ROUTES } from './shared/constants/index';
 import { PublicLayoutComponent } from './shared/components/layouts/public-layout/public-layout.component';
 import { PrivateLayoutComponent } from './shared/components/layouts/private-layout/private-layout.component';
-import { DasboardComponent } from './features/dashboard-management/dasboard/dasboard.component';
 import { guestGuard } from './core/guards/guest.guard';
 import { authGuard } from './core/guards/auth.guard';
 
@@ -33,60 +32,6 @@ export const routes: Routes = [
     canActivate: [authGuard],
     children: [
       {
-        path: ROUTE_BASE_PATHS.DASHBOARD,
-        component: DasboardComponent
-      },
-      {
-        path: ROUTE_BASE_PATHS.EMPLOYEE,
-        loadChildren: () => import('./features/employee-management/employee-management.routes')
-          .then(m => m.EMPLOYEE_MANAGEMENT_ROUTES)
-      },
-      {
-        path: ROUTE_BASE_PATHS.ATTENDANCE,
-        loadChildren: () => import('./features/attendance-management/attendance-management.routes')
-          .then(m => m.ATTENDANCE_MANAGEMENT_ROUTES)
-      },
-      {
-        path: ROUTE_BASE_PATHS.EXPENSE,
-        loadChildren: () => import('./features/regular-expense-management/regular-expense-management.routes')
-          .then(m => m.REGULAR_EXPENSE_MANAGEMENT_ROUTES)
-      },
-      {
-        path: ROUTE_BASE_PATHS.FUEL,
-        loadChildren: () => import('./features/fuel-expense-management/fuel-expense-management.routes')
-          .then(m => m.FUEL_EXPENSE_MANAGEMENT_ROUTES)
-      },
-      {
-        path: ROUTE_BASE_PATHS.SITE.BASE,
-        loadChildren: () => import('./features/site-management/site-management.routes')
-          .then(m => m.SITE_MANAGEMENT_ROUTES)
-      },
-      {
-        path: ROUTE_BASE_PATHS.LEAVE,
-        loadChildren: () => import('./features/leave-management/leave-management.routes')
-          .then(m => m.LEAVE_MANAGEMENT_ROUTES)
-      },
-      {
-        path: ROUTE_BASE_PATHS.CALENDAR,
-        loadChildren: () => import('./features/leave-planner-management/leave-planner-management.routes')
-          .then(m => m.LEAVE_PLANNER_MANAGEMENT_ROUTES)
-      },
-      {
-        path: ROUTE_BASE_PATHS.ASSET,
-        loadChildren: () => import('./features/asset-management/asset-management.routes')
-          .then(m => m.ASSET_MANAGEMENT_ROUTES)
-      },
-      {
-        path: ROUTE_BASE_PATHS.VEHICLE,
-        loadChildren: () => import('./features/vehicle-management/vehicle-management.routes')
-          .then(m => m.VEHICLE_MANAGEMENT_ROUTES)
-      },
-      {
-        path: ROUTE_BASE_PATHS.CARD,
-        loadChildren: () => import('./features/petro-card-management/petro-card-management.routes')
-          .then(m => m.PETRO_CARD_MANAGEMENT_ROUTES)
-      },
-      {
         path: ROUTE_BASE_PATHS.SETTINGS.BASE,
         loadChildren: () => import('./features/settings-management/setting-management.routes')
           .then(m => m.SETTING_MANAGEMENT_ROUTES)
@@ -95,6 +40,10 @@ export const routes: Routes = [
         path: ROUTE_BASE_PATHS.PERMISSION,
         loadChildren: () => import('./features/setting-management--1/setting-management.routes')
           .then(m => m.SETTING_MANAGEMENT_ROUTES)
+      },
+      {
+        path: '**',
+        redirectTo: `${ROUTE_BASE_PATHS.SETTINGS.BASE}`
       }
     ]
   },
