cebf1e7d70ab97a107f6cb6dbfa42cab589f7776
Nikhil Sachan <nikhilsachan159@gmail.com>
2025-07-05 22:45:49 +0530

    Role -set permisison UI work

--- src/app/features/settings-management/permission-management/role-management/components/set-role-permission/set-role-permission.component.ts
+++ src/app/features/settings-management/permission-management/role-management/components/set-role-permission/set-role-permission.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { SetPermissionComponent } from "../../../shared/set-permission/set-permission.component";
+import { IPageHeaderConfig } from '../../../../../../shared/models/page-header-config.model';
+import { PageHeaderComponent } from "../../../../../../shared/components/page-header/page-header.component";
 
 @Component({
   selector: 'app-set-role-permission',
-  imports: [SetPermissionComponent],
+  imports: [SetPermissionComponent, PageHeaderComponent],
   templateUrl: './set-role-permission.component.html',
   styleUrl: './set-role-permission.component.scss'
 })
 export class SetRolePermissionComponent {
+
+  protected pageHeaderConfig = computed<Partial<IPageHeaderConfig>>(() =>
+    this.getPageHeaderConfig(),
+  );
+
+  private getPageHeaderConfig(): Partial<IPageHeaderConfig> {
+    return {
+      title: 'Set Role Permissions',
+      subtitle: 'Set the permissions for the role',
+    };
+  }
 
 }
