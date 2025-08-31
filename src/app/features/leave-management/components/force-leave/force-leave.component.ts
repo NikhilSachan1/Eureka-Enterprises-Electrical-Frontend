4a9f09b6b87ebf1288b12f32950570d459675cb0
Nikhil Sachan <nikhilsachan159@gmail.com>
2025-08-31 16:36:47 +0530

    setup leave management

---
+++ src/app/features/leave-management/components/force-leave/force-leave.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-force-leave',
+  imports: [],
+  templateUrl: './force-leave.component.html',
+  styleUrl: './force-leave.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ForceLeaveComponent {}
---
+++ src/app/features/leave-management/components/get-leave/get-leave.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-leave',
+  imports: [],
+  templateUrl: './get-leave.component.html',
+  styleUrl: './get-leave.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GetLeaveComponent {}
