c2f63d64d9f817360c39c34fb9daf2c6a97c7a1f
Nikhil Sachan <nikhilsachan159@gmail.com>
2026-01-04 13:31:57 +0530

    Add button severity configurations to stepper and enhance form styling with gradients and shadows for improved UI consistency

--- src/app/shared/config/stepper.config.ts
+++ src/app/shared/config/stepper.config.ts
@@ -4,6 +4,7 @@ import {
   EButtonActionType,
   EButtonIconPosition,
   EButtonVariant,
+  EButtonSeverity,
 } from '@shared/types';
 import { ICONS } from '@shared/constants';
 
@@ -13,6 +14,7 @@ export const DEFAULT_STEPPER_NEXT_BUTTON_CONFIG = {
   icon: ICONS.COMMON.ARROW_RIGHT,
   iconPosition: EButtonIconPosition.RIGHT,
   variant: EButtonVariant.OUTLINED,
+  severity: EButtonSeverity.PRIMARY,
 };
 
 export const DEFAULT_STEPPER_BACK_BUTTON_CONFIG = {
@@ -21,12 +23,14 @@ export const DEFAULT_STEPPER_BACK_BUTTON_CONFIG = {
   icon: ICONS.COMMON.ARROW_LEFT,
   iconPosition: EButtonIconPosition.LEFT,
   variant: EButtonVariant.OUTLINED,
+  severity: EButtonSeverity.SECONDARY,
 };
 
 export const DEFAULT_STEPPER_RESET_BUTTON_CONFIG = {
   id: EButtonActionType.RESET,
   label: 'Reset',
   variant: EButtonVariant.OUTLINED,
+  severity: EButtonSeverity.SECONDARY,
 };
 
 export const DEFAULT_STEPPER_CONFIG: Partial<IStepperConfig> = {
