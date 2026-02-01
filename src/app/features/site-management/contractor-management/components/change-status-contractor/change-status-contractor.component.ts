0fa2616ea6b281096bb1c2544c1527546068c37c
Nikhil Sachan <nikhilsachan159@gmail.com>
2026-02-01 11:25:19 +0530

    Contractor setup done

---
+++ src/app/features/site-management/contractor-management/components/change-status-contractor/change-status-contractor.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-status-contractor',
+  imports: [],
+  templateUrl: './change-status-contractor.component.html',
+  styleUrl: './change-status-contractor.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ChangeStatusContractorComponent {}
