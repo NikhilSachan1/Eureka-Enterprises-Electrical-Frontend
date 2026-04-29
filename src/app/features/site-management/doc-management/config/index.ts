d1c2a3188d9f37b76a5277cfe3ff5b8990215d35
Nikhil Sachan <nikhilsachan159@gmail.com>
2026-04-29 21:19:31 +0530

    Refactor confirmation dialog and document management configurations

--- src/app/features/site-management/doc-management/config/index.ts
+++ src/app/features/site-management/doc-management/config/index.ts
@@ -1,8 +1,5 @@
 export { DOC_ACTION_CONFIG_MAP } from './dialog/get-doc.config';
-export {
-  DOC_ADD_BUTTON_CONFIG_MAP,
-  type TDocAddDialogType,
-} from './dialog/get-doc.config';
+export { DOC_ADD_BUTTON_CONFIG_MAP } from './dialog/get-doc.config';
 export { DOC_TABLE_ENHANCED_CONFIG } from './table/get-doc.config';
 export { PO_DOC_FORM_CONFIG } from './form/po-doc.config';
 export { JMC_DOC_FORM_CONFIG } from './form/jmc-doc.config';
