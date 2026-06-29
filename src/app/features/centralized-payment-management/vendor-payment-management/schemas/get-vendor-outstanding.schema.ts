765b970802489ce22ff707f0090a760770d33796
Nikhil Sachan <84980612+NikhilSachan1@users.noreply.github.com>
2026-06-29 23:48:56 +0530

    Enhance vendor outstanding schema with new structures

--- src/app/features/centralized-payment-management/vendor-payment-management/schemas/get-vendor-outstanding.schema.ts
+++ src/app/features/centralized-payment-management/vendor-payment-management/schemas/get-vendor-outstanding.schema.ts
@@ -1,4 +1,9 @@
-import { FilterSchema, uuidField } from '@shared/schemas';
+import {
+  FilterSchema,
+  isoDateTimeField,
+  onlyDateStringField,
+  uuidField,
+} from '@shared/schemas';
 import z from 'zod';
 
 const { sortOrder, sortField, pageSize, page, search } = FilterSchema.shape;
@@ -14,21 +19,90 @@ export const VendorOutstandingGetRequestSchema = z
   .strict();
 
 export const VendorOutstandingBankDetailsSchema = z.looseObject({
-  bankHolderName: z.string().nullable(),
+  accountHolderName: z.string().nullable(),
   bankName: z.string().nullable(),
   accountNumber: z.string().nullable(),
   ifscCode: z.string().nullable(),
 });
 
+export const VendorOutstandingVendorSchema = z.looseObject({
+  id: uuidField,
+  name: z.string(),
+  city: z.string(),
+  state: z.string(),
+  bankDetails: VendorOutstandingBankDetailsSchema,
+});
+
+export const VendorOutstandingVendorSummarySchema = z.looseObject({
+  totalNetPayableAmount: z.number(), //itna booked amount hai
+  totalPaymentAmount: z.number(), //itna paid amount hai jo karna hai
+  totalPendingToBook: z.number(),
+});
+
+export const VendorOutstandingSiteSchema = z.looseObject({
+  id: uuidField,
+  name: z.string(),
+  city: z.string(),
+  state: z.string(),
+});
+
+export const VendorOutstandingCompanySchema = z.looseObject({
+  id: uuidField,
+  name: z.string(),
+});
+
+export const VendorOutstandingJmcSchema = z.looseObject({
+  id: uuidField,
+  jmcNumber: z.string(),
+  jmcDate: isoDateTimeField,
+});
+
+export const VendorOutstandingPoSchema = z.looseObject({
+  id: uuidField,
+  poNumber: z.string(),
+  poDate: isoDateTimeField,
+  totalAmount: z.number(),
+});
+
+export const VendorOutstandingBookPaymentInvoiceSchema = z.looseObject({
+  id: uuidField,
+  invoiceNumber: z.string(),
+  invoiceDate: isoDateTimeField,
+  totalAmount: z.number(),
+});
+
+export const VendorOutstandingBookPaymentSchema = z.looseObject({
+  id: uuidField,
+  bookingDate: onlyDateStringField,
+  paymentTotalAmount: z.number(),
+  invoice: VendorOutstandingBookPaymentInvoiceSchema,
+  jmc: VendorOutstandingJmcSchema,
+  po: VendorOutstandingPoSchema,
+  site: VendorOutstandingSiteSchema,
+  company: VendorOutstandingCompanySchema,
+});
+
+export const VendorOutstandingUnbookedInvoiceSchema = z.looseObject({
+  id: uuidField,
+  invoiceNumber: z.string(),
+  invoiceDate: isoDateTimeField,
+  totalAmount: z.number(),
+  jmc: VendorOutstandingJmcSchema,
+  po: VendorOutstandingPoSchema,
+  site: VendorOutstandingSiteSchema,
+  company: VendorOutstandingCompanySchema,
+});
+
 export const VendorOutstandingGetBaseResponseSchema = z.looseObject({
-  vendorId: uuidField,
-  vendorName: z.string(),
-  pendingAmount: z.number(),
-  bankDetails: VendorOutstandingBankDetailsSchema.nullable(),
+  vendor: VendorOutstandingVendorSchema,
+  vendorSummary: VendorOutstandingVendorSummarySchema,
+  bookPayments: z.array(VendorOutstandingBookPaymentSchema),
+  unbookedInvoices: z.array(VendorOutstandingUnbookedInvoiceSchema),
 });
 
 export const VendorOutstandingGetStatsResponseSchema = z.looseObject({
-  totalPendingAmount: z.number(),
+  totalPaymentAmount: z.number(),
+  totalPendingToBook: z.number(),
 });
 
 export const VendorOutstandingGetResponseSchema = z.looseObject({
